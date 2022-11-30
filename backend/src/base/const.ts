3d64a1a2b7e231a46bd915cb7711d84a967bd069
Nghiem Duong <nghiemduong@Nghiems-MacBook-Pro.local>
2022-11-30 11:08:07 +0700

    Fix: Missing author validation for update and remove post

--- backend/src/base/const.ts
+++ backend/src/base/const.ts
@@ -1,6 +1,13 @@
-export const userSelect = { id: true, email: true, name: true, role: true, hash: true }
+export const userSelect = {
+  id: true,
+  email: true,
+  name: true,
+  role: true,
+  hash: true,
+};
+
 export enum Role {
-  USER = "USER",
-  MODDER = "MODDER",
-  ADMIN = "ADMIN"
+  USER = 'USER',
+  MODDER = 'MODDER',
+  ADMIN = 'ADMIN',
 }
